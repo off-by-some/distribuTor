2bcbcf20327f93486dd850c412607877b9a88d24
Pholey <pholeyoh@gmail.com>
2016-02-06 19:54:13 -0800

    Fix console display :bug:, formatting, catch uncaught errors

--- shutdown/shutdown.go
+++ shutdown/shutdown.go
@@ -1,33 +1,37 @@
 package shutdown
 
 import (
-  t "DistribuTor/torutil"
-  "DistribuTor/db"
-  "fmt"
+	"DistribuTor/db"
+	t "DistribuTor/torutil"
+	"fmt"
 )
 
 func ShutdownNodes() {
-  sql := `SELECT control_port
+	sql := `SELECT control_port
           FROM connection`
 
-  rows, _ := db.Client.Query(sql)
-  for rows.Next() {
-    var port int
-    rows.Scan(&port)
-    fmt.Printf("Closing node at port %d\n", port)
-    t.Shutdown(port)
-  }
+	rows, err := db.Client.Query(sql)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for rows.Next() {
+		var port int
+		rows.Scan(&port)
+		fmt.Printf("Closing node at port %d\n", port)
+		t.Shutdown(port)
+	}
 }
 
 func DropDB() {
-  sql := `DELETE * FROM connection`
-  fmt.Printf("Dropping connection info\n")
-  db.Client.Query(sql)
+	sql := `DELETE * FROM connection`
+	fmt.Printf("Dropping connection info\n")
+	db.Client.Query(sql)
 }
 
 func Shutdown() {
-  fmt.Println("Shutting down nodes")
-  ShutdownNodes()
-  DropDB()
-  fmt.Println("( ͡° ͜ʖ ͡°)")
+	fmt.Println("\nShutting down nodes")
+	ShutdownNodes()
+	DropDB()
 }
